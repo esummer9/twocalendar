bec718b9fd9c45611887a3db4bf7408593605f33
ecodoobiz.com 성수 <dhlee@ecodoobiz.com>
2025-11-24 14:18:04 +0900

    deploy : googleplay 정책관련 512 아이콘 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ediapp.twocalendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
