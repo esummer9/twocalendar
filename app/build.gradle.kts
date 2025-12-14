af741f9731bc9a6b62630495e93ef52fa8fe0221
홈 <esummer.lee@gmail.com>
2025-12-15 05:28:35 +0900

    deploy : 12 (1.1.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ediapp.twocalendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.3"
+        versionCode = 12
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
